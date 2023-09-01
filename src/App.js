e369950f73b320b418b9ad2cd1f1686ece67850d
Nicolas <nicolaspetcoff@gmail.com>
2023-09-01 09:42:27 -0300

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import ResponsiveAppBar from './componentes/Barra';
 
 function App() {
   return (
     <div className="App">
     
-    mas cambios
-
-    
+    <ResponsiveAppBar/>    
 
 
     </div>

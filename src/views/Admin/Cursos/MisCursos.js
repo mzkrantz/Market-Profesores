09dc49ae57a1583406129eb2d4a18c1902b3d903
nicopetcoff <nicolaspetcoff@MacBook-Air-de-Nicolas.local>
2023-10-11 19:20:36 -0300

    tabla

--- src/views/Admin/Cursos/MisCursos.js
+++ src/views/Admin/Cursos/MisCursos.js
@@ -9,24 +9,26 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { styled } from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "../TableStyles.css";
 import SpacerTop from "../../../componentes/Spacer/SpacerTop";
 import EditCursoForm from "../../../componentes/Forms/EditCursoForm";
 
-// Simulacion datos de cursos del profesor, cargar desde el json? Mock=simulacion
 const mockCourses = [
   {
     id: 1,
     image: "curso-programacion.jpg",
     title: "Desarrollo Web Avanzado",
-    description: "Domina el desarrollo web con tecnologías modernas.",
+    description: "Domina el desarrollo web con tecnologías modernas. Aprende las mejores prácticas y las últimas tendencias en desarrollo web para construir aplicaciones web impresionantes. Este curso te proporcionará las habilidades necesarias para destacarte en la industria del desarrollo web, cubriendo temas como HTML, CSS, JavaScript, frameworks modernos y metodologías de desarrollo ágiles.",
     duration: "12",
     frequency: "2",
     price: "199.99",
     buttonLink: "https://ejemplo.com/curso-desarrollo-web-avanzado",
     category: "Desarrollo Web",
-    extendedDescription:
-      "Domina el desarrollo web con tecnologías modernas y conviértete en un experto en el campo.",
+    extendedDescription: "Domina el desarrollo web con tecnologías modernas y conviértete en un experto en el campo.",
     subjects: ["HTML", "CSS", "JavaScript"],
     stars: "4.5",
     type: "Grupal",
@@ -37,14 +39,13 @@ const mockCourses = [
     id: 2,
     image: "curso-marketing.jpg",
     title: "Marketing Digital Estratégico",
-    description: "Aprende a crear campañas de marketing efectivas.",
+    description: "Aprende a crear campañas de marketing efectivas. Adquiere conocimientos en estrategias de marketing digital que te permitirán diseñar y ejecutar campañas exitosas. Conviértete en un especialista en marketing digital y maximiza el impacto de tu estrategia, explorando las últimas tendencias en marketing digital, incluyendo SEO, publicidad en línea, marketing en redes sociales y más.",
     duration: "8",
     frequency: "3",
     price: "149.99",
     buttonLink: "https://ejemplo.com/curso-marketing-digital",
     category: "Marketing Digital",
-    extendedDescription:
-      "Aprende a crear campañas de marketing efectivas y maximiza el impacto de tu estrategia.",
+    extendedDescription: "Aprende a crear campañas de marketing efectivas y maximiza el impacto de tu estrategia.",
     subjects: ["Marketing en redes sociales", "SEO", "Publicidad en línea"],
     stars: "4",
     type: "Individual",
@@ -55,14 +56,13 @@ const mockCourses = [
     id: 3,
     image: "curso-diseno-grafico.jpg",
     title: "Diseño Gráfico Creativo",
-    description: "Crea diseños impactantes y creativos.",
+    description: "Crea diseños impactantes y creativos. Desarrolla tus habilidades de diseño gráfico y descubre tu creatividad. Aprende a diseñar logotipos, ilustraciones y proyectos visualmente atractivos, explorando las técnicas de diseño gráfico, trabajando con herramientas como Adobe Photoshop e Illustrator, y aprendiendo a crear logotipos, ilustraciones y diseños únicos.",
     duration: "10",
     frequency: "2",
     price: "179.99",
     buttonLink: "https://ejemplo.com/curso-diseno-grafico",
     category: "Diseño Gráfico",
-    extendedDescription:
-      "Desarrolla tus habilidades de diseño gráfico y crea proyectos visualmente atractivos.",
+    extendedDescription: "Desarrolla tus habilidades de diseño gráfico y crea proyectos visualmente atractivos.",
     subjects: ["Adobe Photoshop", "Ilustración digital", "Diseño de logotipos"],
     stars: "4.2",
     type: "Grupal",
@@ -73,14 +73,13 @@ const mockCourses = [
     id: 4,
     image: "curso-fotografia.jpg",
     title: "Fotografía Profesional",
-    description: "Captura momentos inolvidables con tu cámara.",
+    description: "Captura momentos inolvidables con tu cámara. Aprende las técnicas esenciales de la fotografía y mejora tus habilidades como fotógrafo, sumergiéndote en el mundo de la fotografía profesional, cubriendo temas como composición, iluminación, edición de fotos y más.",
     duration: "6",
     frequency: "1",
     price: "99.99",
     buttonLink: "https://ejemplo.com/curso-fotografia-profesional",
     category: "Fotografía",
-    extendedDescription:
-      "Aprende las técnicas esenciales de la fotografía y mejora tus habilidades como fotógrafo.",
+    extendedDescription: "Aprende las técnicas esenciales de la fotografía y mejora tus habilidades como fotógrafo.",
     subjects: ["Composición", "Iluminación", "Edición de fotos"],
     stars: "4.7",
     type: "Individual",
@@ -91,14 +90,13 @@ const mockCourses = [
     id: 5,
     image: "curso-idiomas.jpg",
     title: "Aprende un Nuevo Idioma",
-    description: "Domina un idioma extranjero y amplía tus horizontes.",
+    description: "Domina un idioma extranjero y amplía tus horizontes, sumérgete en una nueva cultura aprendiendo un idioma extranjero de manera efectiva, aprendiendo a dominar idiomas como inglés, francés, español y alemán.",
     duration: "16",
     frequency: "4",
     price: "249.99",
     buttonLink: "https://ejemplo.com/curso-aprender-idiomas",
     category: "Idiomas",
-    extendedDescription:
-      "Sumérgete en una nueva cultura aprendiendo un idioma extranjero de manera efectiva.",
+    extendedDescription: "Sumérgete en una nueva cultura aprendiendo un idioma extranjero de manera efectiva.",
     subjects: ["Inglés", "Francés", "Español", "Alemán"],
     stars: "4.9",
     type: "Grupal",
@@ -107,58 +105,56 @@ const mockCourses = [
   },
 ];
 
-
 const CourseList = styled(TableContainer)`
   margin-top: ${({ theme }) => theme.spacing(2)};
-  overflow-x: auto; // Permite desplazamiento horizontal
-  max-width: 100%; // Evita que la tabla se vaya del contenedor
+  overflow-x: auto;
+  max-width: 100%;
 `;
 
 const ResponsiveTable = styled(Table)`
+  background-color: #fff;
+
   @media (max-width: 600px) {
-    font-size: 12px; // Reducir el tamaño del texto en las celdas para pantallas pequeñas
+    font-size: 20px;
     table {
-      width: 100%; /* Asegurar que la tabla ocupe todo el ancho disponible */
+      width: 100%;
     }
 
     th,
     td {
-      display: flex; /* Convertir celdas en elementos flex (apilados) */
-      flex-direction: column;
-      text-align: center; /* Alinear el texto a la izquierda */
-      margin-bottom: 10px; /* Agregar un espacio entre las celdas */
+      text-align: center;
     }
 
     td {
-      border-bottom: 1px solid #ddd; /* Agregar separadores entre las filas */
-      padding: 8px; /* Espaciado interno para las celdas */
-    }
-
-    td:last-child {
-      border-bottom: 1px solid #ddd; /* Agregar separadores entre las filas */
-      margin-bottom: 1rem; /* Agregar un espacio entre las celdas */
-      padding-bottom: 2rem; /* Agregar un espacio entre las celdas */
-      border-bottom: 2px solid grey; /* Agregar separadores entre las filas */
+      padding: 8px;
+      border: none; // Quitamos el borde
     }
   }
 `;
 
+const ButtonContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 5px;
+  align-items: center; // Centramos los botones
+`;
+
 const MisCursos = () => {
   const [courses, setCourses] = useState([]);
-  const [isFormOpen, setIsFormOpen] = useState(false); // Estado para controlar si el formulario está abierto
+  const [isFormOpen, setIsFormOpen] = useState(false);
   const [editingCourse, setEditingCourse] = useState(null);
 
-  // Simulacion de la carga de datos
   useEffect(() => {
     setCourses(mockCourses);
   }, []);
 
   const openForm = () => {
-    setIsFormOpen(true); // Abre el formulario al hacer clic en "Crear Nuevo Curso"
+    setIsFormOpen(true);
   };
 
   const closeForm = () => {
-    setIsFormOpen(false); // Cierra el formulario
+    setIsFormOpen(false);
   };
 
   return (
@@ -172,9 +168,9 @@ const MisCursos = () => {
             variant="contained"
             color="primary"
             onClick={() => {
-              setEditingCourse(); // Establece el curso que se va a editar
-              openForm(); // Abre el formulario de creacion
-            }} // Abre el formulario al hacer clic
+              setEditingCourse();
+              openForm();
+            }}
           >
             Crear Nuevo Curso
           </Button>
@@ -191,50 +187,46 @@ const MisCursos = () => {
             </TableHead>
             <TableBody>
               {courses.map((course) => (
-                <TableRow key={course.id}>
-                  <TableCell>{course.title}</TableCell>
+                <TableRow  key={course.id} >
+                  <TableCell >{course.title}</TableCell>
                   <TableCell>{course.description}</TableCell>
                   <TableCell>
                     {course.published ? "Publicado" : "No Publicado"}
                   </TableCell>
-                  <TableCell
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <Button
-                      className="boton-tabla"
-                      variant="outlined"
-                      onClick={() => {
-                        setEditingCourse(course); // Establece el curso que se va a editar
-                        openForm(); // Abre el formulario de edición
-                      }}
-                    >
-                      Editar
-                    </Button>
-                    <Button
-                      className="boton-tabla"
-                      variant="outlined"
-                      onClick={() => {
-                        // Publicar o despublicar el curso
-                        alert(
-                          "Implementar la lógica de publicación/despublicación"
-                        );
-                      }}
-                    >
-                      {course.published ? "Despublicar" : "Publicar"}
-                    </Button>
-                    <Button
-                      className="boton-tabla"
-                      variant="outlined"
-                      color="error"
-                      onClick={() => {
-                        // Eliminar el curso
-                        alert("Implementar la lógica de eliminación");
-                      }}
-                    >
-                      Eliminar
-                    </Button>
+                  <TableCell>
+                    <ButtonContainer>
+                      <IconButton
+                        className="boton-tabla"
+                        onClick={() => {
+                          setEditingCourse(course);
+                          openForm();
+                        }}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </ButtonContainer >
+                    <ButtonContainer>
+                      <IconButton
+                        className="boton-tabla"
+                        onClick={() => {
+                          alert("Implementar la lógica de publicación/despublicación");
+                        }}
+                      >
+                        <VisibilityIcon />
+                      </IconButton>
+                    </ButtonContainer>
+                    <ButtonContainer>
+                      <IconButton
+                        className="boton-tabla"
+                        onClick={() => {
+                          alert("Implementar la lógica de eliminación");
+                        }}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </ButtonContainer>
                   </TableCell>
-                </TableRow>
+                </TableRow >
               ))}
             </TableBody>
           </ResponsiveTable>
